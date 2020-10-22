6a8ba95ae5283a2b68fd84355f76d8368abfda50
John King <1070631+jejking@users.noreply.github.com>
2020-10-22 18:59:17 +0200

    updates dependencies, gradle version

--- build.gradle.kts
+++ build.gradle.kts
@@ -2,17 +2,17 @@ import com.github.benmanes.gradle.versions.updates.DependencyUpdatesTask
 
 val detektVersion = "1.9.1"
 val junitJupiterVersion = "5.7.0"
-val koTestVersion = "4.2.5"
+val koTestVersion = "4.3.0"
 val kotlinxCoRoutinesVersion = "1.3.9"
 
 val sourceCompatibility = JavaVersion.VERSION_11
 
 plugins {
   val kotlinVersion = "1.4.10"
-  val ktlintVersion = "9.4.0"
-  val testLoggerVersion = "2.1.0"
+  val ktlintVersion = "9.4.1"
+  val testLoggerVersion = "2.1.1"
   val versionsVersion = "0.33.0"
-  val detektVersion = "1.14.0"
+  val detektVersion = "1.14.2"
   // Apply the Kotlin JVM plugin to add support for Kotlin.
   id("org.jetbrains.kotlin.jvm") version kotlinVersion
   id("org.jlleitschuh.gradle.ktlint") version ktlintVersion
